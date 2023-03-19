d95efbd421297e8c5215f0243c3cde324b2550de
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:40:45 +0900

    Update Climbing_Stairs.py

--- Climbing_Stairs.py
+++ Climbing_Stairs.py
@@ -1,3 +1,8 @@
+# LEETCODE PROBLEM: 
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         a = [0 for i in range(n+1)]
