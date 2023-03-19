75eb2df8acae1dcf3f4371a78d6265c6433ae59a
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:52:53 +0900

    Update Swap_Nodes_in_Pairs.py

--- Swap_Nodes_in_Pairs.py
+++ Swap_Nodes_in_Pairs.py
@@ -1,3 +1,9 @@
+# LEETCODE PROBLEM:
+# Given a linked list, swap every two adjacent nodes and return its head.
+# You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
