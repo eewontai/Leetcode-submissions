2108b5e33bc2dc57a4ca2009681e6b9f7efe0832
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:44:29 +0900

    Update Flatten_Binary_Tree_to_Linked_List.py

--- Flatten_Binary_Tree_to_Linked_List.py
+++ Flatten_Binary_Tree_to_Linked_List.py
@@ -1,3 +1,10 @@
+# LEETCODE PROBLEM: 
+# Given the root of a binary tree, flatten the tree into a "linked list":
+# The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+# The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
