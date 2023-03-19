aed4b723d709421e67e382d0467afd45841d5acd
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:42:38 +0900

    Update Find_the_Index_of_the_First_Occurrence_in_a_String.py

--- Find_the_Index_of_the_First_Occurrence_in_a_String.py
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -1,3 +1,11 @@
+# LEETCODE PROBLEM:
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Constraints:
+# 1 <= haystack.length, needle.length <= 104
+# haystack and needle consist of only lowercase English characters.
+
+
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == "":
