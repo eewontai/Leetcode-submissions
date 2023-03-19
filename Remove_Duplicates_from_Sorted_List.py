8a9d2cf6abf6d968fb6a525e7066c590e0a03806
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:50:07 +0900

    Update Remove_Duplicates_from_Sorted_List.py

--- Remove_Duplicates_from_Sorted_List.py
+++ Remove_Duplicates_from_Sorted_List.py
@@ -1,3 +1,12 @@
+# LEETCODE PROBLEM:
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+# Constraints:
+# The number of nodes in the list is in the range [0, 300].
+# -100 <= Node.val <= 100
+# The list is guaranteed to be sorted in ascending order.
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
