f8494334bb887d3bdf7ad2688e5b2dd8d9b1aefa
eewontai <82491733+eewontai@users.noreply.github.com>
2023-03-19 18:41:19 +0900

    Update Closest_Binary_Search_Tree_Value.py

--- Closest_Binary_Search_Tree_Value.py
+++ Closest_Binary_Search_Tree_Value.py
@@ -1,3 +1,10 @@
+# LEETCODE PROBLEM: 
+# Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+# Note:
+# Given target value is a floating point.
+# You are guaranteed to have only one unique value in the BST that is closest to the target.
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
